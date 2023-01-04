2142a53685abb338cea2d24758e2dc104a2ba3eb
Abdulhamid Ibrahim <ibrahimabdulhamid04@gmail.com>
2023-01-04 10:33:17 +0100

    Delete 0-positive_or_negative.py

--- 0-positive_or_negative.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print(f"{number:d} is positive")
-elif number == 0:
-    print(f"{number:d} is zero")
-else:
-    print(f"{number:d} is negative")
